3b012b1b180f2e7816e75f6dba7b62e59c7d54cf
Dailius <dailius.rascius@gmail.com>
2025-06-04 11:38:03 +0300

    Update express_user_mng project

---
+++ notes/lecture_22.md
@@ -0,0 +1,13 @@
+1. Repeat and check homework
+2. Node express User Auth project 
+
+
+## 1. Repeat and check homework
+
+## 2. Node express User Auth project 
+
+```javascript
+```
+
+```javascript
+```
--- projects/08_experss_usr_mng/app.js
+++ projects/08_experss_usr_mng/app.js
@@ -28,10 +28,41 @@ app.post("/v1/users", (req, res) => {
         };
 
         // username validation: unique
+        if (db.users.find(it => it.username === user.username)) {
+            return res.status(409).json({
+                message: "Username already exist"
+            });
+        };
+
         // username validation: min 3, max 35
+        if (user.username.length < 3 || user.username.length > 35) {
+            return res.status(400).json({
+                message: "Username length min 3 and max 35)"
+            });
+        }
+
         // email validation: unique
+        if (db.users.find(it => it.email === user.email)) {
+            return res.status(409).json({
+                message: "Email already exist"
+            });
+        }
+
         // email validation: keep email format
+        // if (!user.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
+            return res.status(400).json({
+                message: "Invalid email format"
+            });
+        }
+
         // password validation: length 8 and more, only letters and numbers
+        // if (user.password.length < 8 || !user.password.match(/^[A-Za-z0-9]+$/)) {
+        if (user.password.length < 8 || !user.password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9]+$/)) {
+            return res.status(400).json({
+                message: "Invalid password"
+            });
+        }
 
         db.max_id = user.id;
         db.users.push(user);
@@ -62,6 +93,11 @@ app.get("/v1/users/:id", (req, res) => {
 
     res.status(200).json(db.users[index]);
 });
+
+// http://localhost:3011/v1/login
+// login
+    // authorization: check email and password.
+    // return message that user signed in.  
 
 // put method with validations:
     // email validation: unique
