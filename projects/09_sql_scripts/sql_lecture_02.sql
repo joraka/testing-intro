43e6fd8900c924922199df004192854c7b8e7860
Dailius <dailius.rascius@gmail.com>
2025-06-11 11:44:21 +0300

    Update lecture 26

--- projects/09_sql_scripts/sql_lecture_02.sql
+++ projects/09_sql_scripts/sql_lecture_02.sql
@@ -35,6 +35,70 @@
 	WHERE 'Behind the Scenes' = ANY(special_features);
 	-- WHERE special_features[1] = 'Behind the Scenes';
 
+/*
+	MySQL date_formatter()
+	PostgreSQL to_char()
+*/
+
+	SELECT * FROM rental;
+
+	SELECT rental_id, rental_date, date_part('day', rental_date), date_part('year', rental_date) FROM rental;
+	SELECT rental_id, rental_date FROM rental WHERE date_part('year', rental_date) = 2005;
+	SELECT rental_id, rental_date FROM rental WHERE date_part('year', rental_date) = 2005 AND date_part('month', rental_date) = 6;
+
+	
+	SELECT rental_id, rental_date FROM rental WHERE to_char(rental_date, 'YYYY') = '2006';
+	SELECT rental_id, rental_date FROM rental WHERE to_char(rental_date, 'MM') = '06';
+	SELECT rental_id, rental_date FROM rental WHERE to_char(rental_date, 'YYYY-MM') = '2005-05';
+	
+	SELECT rental_id, rental_date, to_char(rental_date, 'HH -> HH24') FROM rental WHERE to_char(rental_date, 'YYYY MM HH24') = '2005 05 23';
+
+	SELECT rental_id, rental_date FROM rental 
+	WHERE CAST(to_char(rental_date, 'YYYY') AS INTEGER) = 2006;
+
+/*
+	JOIN
+*/
+
+	SELECT * FROM film; -- film_id
+	SELECT * FROM film_category; -- film_id, category_id
+	SELECT * FROM category; -- category_id
+
+	SELECT 
+		f.film_id,
+		f.title,
+		fc.category_id,
+		c.name
+	FROM film f
+	JOIN film_category fc ON f.film_id = fc.film_id
+	JOIN category c ON c.category_id = fc.category_id
+	WHERE c.name = 'Documentary';
+	
+	SELECT * FROM film; 		-- language_id
+	SELECT * FROM language; 	-- language_id
+
+--  INNER JOIN
+	SELECT 
+		film_id,
+		title,
+		f.language_id,
+		name
+	FROM film f
+	JOIN language l ON l.language_id = f.language_id
+	-- WHERE l.name NOT IN ('English');
+
+
+--  LEFT JOIN
+	SELECT 
+		film_id,
+		title,
+		f.language_id,
+		name
+	FROM language l
+	-- LEFT JOIN film f ON l.language_id = f.language_id
+	RIGHT JOIN film f ON l.language_id = f.language_id
+	-- WHERE l.name NOT IN ('English');
+	
 
 	
 	
